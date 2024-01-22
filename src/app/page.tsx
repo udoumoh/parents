4cc54f6d5127ca293ec59d3427a22734b5cecd8a
John <johnudoumoh63@gmail.com>
2024-01-22 17:49:14 +0100

    add hover for chat

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { redirect } from 'next/navigation'
+
+interface homeProps {
+  
+}
+
+const Home: FC<homeProps> = ({}) => {
+  redirect('/dashboard/overview')
+}
+
+export default Home
