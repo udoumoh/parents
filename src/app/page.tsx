a146757bc87ef2de57c447852eec8ed765c3deb0
John <johnudoumoh63@gmail.com>
2024-02-21 08:19:02 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,30 @@
 'use client'
-import { FC, useEffect  } from 'react'
-import { redirect } from 'next/navigation'
-import { usePathname } from 'next/navigation'
+import { useEffect  } from 'react'
 import { useRouter } from 'next/navigation'
 import { GET_PARENT } from '@/gql/queries/queries'
 import { useQuery } from "@apollo/client";
 
 
 const Home = ({}) => {
-  const path = usePathname();
   const router = useRouter();
   const { data: parent } = useQuery(GET_PARENT);
-  const isPublicRoute = ["/signin", "/signup", "/verifyotp"].includes(path);
 
-  const response = parent || [];
-  console.log(response?.parent?.errors);
-
-  // Check for initial redirect when the component mounts
-  useEffect(() => {
-    if (response?.parent?.errors) {
-      redirect("/signin");
-    } else {
-      redirect("/dashboard/overview");
-    }
-  }, [response?.parent?.errors, router, isPublicRoute]);
+ useEffect(() => {
+   const fetchData = async () => {
+     try {
+       const response = (await parent) || [];
+       if (response.parent.errors === null || !response) {
+         router.push("/signin");
+       } else {
+        router.push("/dashboard/overview")
+       }
+     } catch (error) {
+       console.error("Error fetching data:", error);
+       router.push("/signin");
+     }
+   };
+   fetchData();
+ }, [parent, router]);
 }
 
 export default Home;
