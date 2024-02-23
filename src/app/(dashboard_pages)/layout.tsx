3bc1cfc96064ad107209b87628180c8f11d97ec6
John <johnudoumoh63@gmail.com>
2024-02-23 18:09:49 +0100

    .

--- src/app/(dashboard_pages)/layout.tsx
+++ src/app/(dashboard_pages)/layout.tsx
@@ -1,12 +1,20 @@
 'use client'
 import React, { ReactNode, useEffect } from 'react';
 import MainNav from '@/components/navigation/mainNav';
+import { useQuery } from '@apollo/client';
+import { GET_PARENT } from '@/gql/queries/queries';
+import { useRouter } from 'next/navigation';
 
 interface layoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<layoutProps> = ({ children }) => {
+      const router = useRouter()
+      const { data: parent, loading } = useQuery(GET_PARENT);
+
+      if (loading) return <p>Loading...</p>;
+      if (!loading && parent.parent.errors !== null) router.push("/signin");
   return (
     <MainNav>
       {/* Your layout content */}
