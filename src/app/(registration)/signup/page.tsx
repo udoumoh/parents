63d4e1ba9c8fb4c8dba4613ae000b1a646e0585f
John <johnudoumoh63@gmail.com>
2024-02-23 18:29:50 +0100

    .

--- src/app/(registration)/signup/page.tsx
+++ src/app/(registration)/signup/page.tsx
@@ -49,11 +49,168 @@ mutation RegisterParent($folder: String!, $options: parentRegInput!) {
       parentRole
       phoneNumber
       email
-      relationToStudent
       role
       folder
       isDisabled
       profileImgUrl
+      children {
+        id
+        createdAt
+        transferedAt
+        firstName
+        middleName
+        lastName
+        gender
+        ageInput
+        folder
+        isOwing
+        isVisible
+        isDuplicate
+        linkedAt
+        linkCount
+        isLinked
+        startDate
+        endDate
+        birthDate
+        isArchived
+        profileImgUrl
+        classroom {
+          classroom {
+            id
+            isValid
+            wasEdited
+            createdAt
+            updatedAt
+            classId
+            className
+            classSubjects
+            description
+            isDisabled
+            students {
+              id
+              createdAt
+              transferedAt
+              firstName
+              middleName
+              lastName
+              gender
+              ageInput
+              folder
+              isOwing
+              isVisible
+              isDuplicate
+              linkedAt
+              linkCount
+              isLinked
+              startDate
+              endDate
+              birthDate
+              isArchived
+              profileImgUrl
+              grayId
+              fatherName
+              fatherEmail
+              fatherNumber
+              motherName
+              motherEmail
+              motherNumber
+              homeAddress
+              lgaOrigin
+              state
+            }
+            teacher {
+              id
+              userId
+              createdAt
+              status
+              firstName
+              middleName
+              lastName
+              phoneNumber
+              email
+              role
+              folder
+              isDisabled
+              isVisible
+              profileImgUrl
+            }
+          }
+        }
+        school {
+          school {
+            id
+            createdAt
+            isDisabled
+            isVerified
+            schoolName
+            rcnumber
+            address
+            type
+            lgarea
+            folder
+            state
+            country
+            description
+            phonenumber
+            email
+            websiteUrl
+            instagramUrl
+            facebookUrl
+            twitterUrl
+            linkedinUrl
+            logoImgUrl
+            bannerImgUrl
+            license
+          }
+        }
+        creator {
+          admin {
+            id
+            isPaid
+            userId
+            folder
+            status
+            plan
+            isReferred
+            isDisabled
+            agreedTo
+            referralCode
+            createdAt
+            firstName
+            middleName
+            lastName
+            phoneNumber
+            email
+            profileImgUrl
+            role
+            school
+            schoolImg
+            statusCode
+          }
+        }
+        studentCase {
+          grayCase {
+            id
+            createdAt
+            updatedAt
+            category
+            owingAmount
+            note
+            isActive
+            wasEdited
+          }
+        }
+        grayId
+        fatherName
+        fatherEmail
+        fatherNumber
+        motherName
+        motherEmail
+        motherNumber
+        homeAddress
+        lgaOrigin
+        state
+      }
     }
   }
 }`);
