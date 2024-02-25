1b9bdb5eb25997008eb16e6c101b20c210ce7e3e
John <johnudoumoh63@gmail.com>
2024-02-25 10:42:46 +0100

    .

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -20,21 +20,21 @@ const Layout: React.FC<layoutProps> = ({ children }) => {
       const router = useRouter()
       const { data: parent, loading } = useQuery(GET_PARENT);
 
-      if (loading) return (
-        <Center>
-          <Box minW="full" mt={{ base: 60, md: 60, lg: 40 }}>
-            <Flex
-              direction="column"
-              align="center"
-              minW={{ base: "full", lg: "650px" }}
-            >
-              <Image src="/images/greylightBordered.svg" alt="logo" w={40} mb={3} />
-              <BarLoader color="#ffd880" width="150px" />
-            </Flex>
-          </Box>
-        </Center>
-      );
-      if (!loading && parent.parent.errors !== null) router.push("/signin");
+      // if (loading) return (
+      //   <Center>
+      //     <Box minW="full" mt={{ base: 60, md: 60, lg: 40 }}>
+      //       <Flex
+      //         direction="column"
+      //         align="center"
+      //         minW={{ base: "full", lg: "650px" }}
+      //       >
+      //         <Image src="/images/greylightBordered.svg" alt="logo" w={40} mb={3} />
+      //         <BarLoader color="#ffd880" width="150px" />
+      //       </Flex>
+      //     </Box>
+      //   </Center>
+      // );
+      // if (!loading && parent.parent.errors !== null) router.push("/signin");
   return (
     <MainNav>
       {/* Your layout content */}
