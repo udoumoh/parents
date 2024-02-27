4934298dfdb1fd6c59a08fd40b445c467e90bec4
John <johnudoumoh63@gmail.com>
2024-02-27 19:39:51 +0100

    .

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -4,7 +4,10 @@ import { redirect } from "next/navigation";
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
-  redirect("/dashboard/home/overview");
+  // redirect("/dashboard/home/overview");
+  return(
+    <></>
+  )
 };
 
 export default Page;
