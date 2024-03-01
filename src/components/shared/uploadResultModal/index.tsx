ab4cb2e3e8ce261db125ed2d151570b00ae85905
John <johnudoumoh63@gmail.com>
2024-03-01 18:37:27 +0100

    .

--- src/components/shared/uploadResultModal/index.tsx
+++ src/components/shared/uploadResultModal/index.tsx
@@ -73,6 +73,8 @@ const UploadResultModal: FC<UploadResultModalProps> = ({
     setSummary(event.target.checked);
   };
 
+console.log(selectedSchool)
+
   const handleCheck = () => {
     setChecked(!isChecked);
     setSelectedSchool({
