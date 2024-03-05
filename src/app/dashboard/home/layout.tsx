b99ec05a72b830b4486ad9ed23a49c3bac53430a
John <johnudoumoh63@gmail.com>
2024-03-05 14:09:49 +0100

    .

--- src/app/dashboard/home/layout.tsx
+++ src/app/dashboard/home/layout.tsx
@@ -1,18 +1,150 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import SidebarWithHeader from "@/components/navigation/secondaryNav";
+import {
+  Center,
+  Box,
+  Flex,
+  Image,
+  useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Button,
+  useDisclosure,
+  Avatar,
+} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import { useUserAPI } from "@/hooks/UserContext";
+import { useQuery } from "@apollo/client";
+import { GET_PARENT } from "@/gql/queries/queries";
+import { capitalizeFirstLetter } from "@/helpers/capitalizeFirstLetter";
+import { BarLoader } from "react-spinners";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  const { parentData } = useUserAPI();
+  const { data: loading } = useQuery(GET_PARENT);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { parentData, childData, setLocalstorageId, currentId } = useUserAPI();
+  const router = useRouter();
+
+  useEffect(() => {
+    const currentId = localStorage.getItem("currentId");
+
+    if (currentId === null) {
+      onOpen(); // Open the modal
+    }
+  }, [onOpen]);
+
+  if (loading) {
+    return (
+      <Center>
+        <Box minW="full" mt={{ base: 60, md: 60, lg: 40 }}>
+          <Flex
+            direction="column"
+            align="center"
+            minW={{ base: "full", lg: "650px" }}
+          >
+            <Image
+              src="/images/greylightBordered.svg"
+              alt="logo"
+              w={40}
+              mb={3}
+              pointerEvents={"none"}
+            />
+            <BarLoader color="#ffd880" width="150px" />
+          </Flex>
+        </Box>
+      </Center>
+    );
+  }
+
   if (parentData?.children.length === 0) {
     window.location.replace("/dashboard");
   }
-  return <SidebarWithHeader>{children}</SidebarWithHeader>;
+
+  const Overlay = () => <ModalOverlay bg="none" backdropFilter="blur(10px)" />;
+  return (
+    <SidebarWithHeader>
+      {/* ... Your modal content ... */}
+      <Modal
+        isCentered
+        isOpen={isOpen}
+        onClose={onClose}
+        closeOnOverlayClick={false}
+      >
+        <Overlay />
+        <ModalContent>
+          <ModalHeader>
+            Hello{" "}
+            {`${capitalizeFirstLetter(parentData?.firstName || "")} ${
+              parentData?.lastName
+            }`}
+            🙃
+          </ModalHeader>
+          <ModalBody>
+            <Text mb={"2rem"}>
+              Kindly select a childs profile to view his/her data
+            </Text>
+
+            {childData?.map((ward: any, index: number) => {
+              console.log(ward.id);
+              return (
+                <Flex
+                  alignItems={"center"}
+                  gap={2}
+                  bgColor={currentId === ward.id ? "#3F999830" : ""}
+                  rounded={"md"}
+                  py={"0.5rem"}
+                  px={"1rem"}
+                  mb={"0.4rem"}
+                  border={"1px solid #1F8E74"}
+                  _hover={{
+                    backgroundColor: "#3F999830",
+                    cursor: "pointer",
+                  }}
+                  key={index}
+                  onClick={() => {
+                    setLocalstorageId(ward?.id || 0);
+                    router.refresh();
+                    onClose();
+                  }}
+                >
+                  <Avatar
+                    size={"md"}
+                    src={ward.profileImage}
+                    pointerEvents={"none"}
+                  />
+                  <Box lineHeight={"20px"}>
+                    <Text fontWeight={"600"} fontSize={"sm"}>
+                      {`${ward.firstName} ${ward.lastName}`}
+                    </Text>
+                    <Text
+                      fontSize={"12px"}
+                      color={"#AAAAAA"}
+                      fontWeight={"600"}
+                    >
+                      {ward.greynoteNumber}
+                    </Text>
+                  </Box>
+                </Flex>
+              );
+            })}
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+      {children}
+    </SidebarWithHeader>
+  );
 };
 
 export default Layout;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,30 +1,11 @@
 'use client'
-import React, { useEffect } from "react";
+import React from "react";
 import MainNav from "@/components/navigation/mainNav";
 import { useQuery } from "@apollo/client";
 import { GET_PARENT } from "@/gql/queries/queries";
 import {
-  Center,
-  Box,
-  Flex,
-  Image,
   useToast,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Text,
-  Button,
-  useDisclosure,
-  Avatar,
 } from "@chakra-ui/react";
-import { BarLoader } from "react-spinners";
-import { useUserAPI } from "@/hooks/UserContext";
-import { useRouter } from "next/navigation";
-import { capitalizeFirstLetter } from "@/helpers/capitalizeFirstLetter";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -33,40 +14,6 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const toast = useToast();
   const { data: parent, loading } = useQuery(GET_PARENT);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const {parentData, childData, setLocalstorageId, currentId} = useUserAPI()
-  const router = useRouter()
-
-  useEffect(() => {
-    const currentId = localStorage.getItem("currentId");
-
-    if (currentId === null) {
-      onOpen(); // Open the modal
-    }
-  }, [onOpen]);
-
-  if (loading) {
-    return (
-      <Center>
-        <Box minW="full" mt={{ base: 60, md: 60, lg: 40 }}>
-          <Flex
-            direction="column"
-            align="center"
-            minW={{ base: "full", lg: "650px" }}
-          >
-            <Image
-              src="/images/greylightBordered.svg"
-              alt="logo"
-              w={40}
-              mb={3}
-              pointerEvents={"none"}
-            />
-            <BarLoader color="#ffd880" width="150px" />
-          </Flex>
-        </Box>
-      </Center>
-    );
-  }
 
   try {
     if (!loading && parent?.parent?.errors !== null) {
@@ -83,79 +30,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     });
   }
 
-   const Overlay = () => (
-     <ModalOverlay
-       bg="none"
-       backdropFilter="blur(10px)"
-     />
-   );
-
   return (
     <MainNav>
-      {/* ... Your modal content ... */}
-      <Modal
-        isCentered
-        isOpen={isOpen}
-        onClose={onClose}
-        closeOnOverlayClick={false}
-      >
-        <Overlay />
-        <ModalContent>
-          <ModalHeader>
-            Hello {`${capitalizeFirstLetter(parentData?.firstName || "")} ${parentData?.lastName}`}🙃
-          </ModalHeader>
-          <ModalBody>
-            <Text mb={"2rem"}>
-              Kindly select a childs profile to view his/her data
-            </Text>
-
-            {childData?.map((ward: any, index: number) => {
-              console.log(ward.id);
-              return (
-                <Flex
-                  alignItems={"center"}
-                  gap={2}
-                  bgColor={currentId === ward.id ? "#3F999830" : ""}
-                  rounded={"md"}
-                  py={"0.5rem"}
-                  px={"1rem"}
-                  mb={"0.4rem"}
-                  border={"1px solid #1F8E74"}
-                  _hover={{
-                    backgroundColor: "#3F999830",
-                    cursor: "pointer",
-                  }}
-                  key={index}
-                  onClick={() => {
-                    setLocalstorageId(ward?.id || 0);
-                    router.refresh();
-                    onClose();
-                  }}
-                >
-                  <Avatar
-                    size={"md"}
-                    src={ward.profileImage}
-                    pointerEvents={"none"}
-                  />
-                  <Box lineHeight={"20px"}>
-                    <Text fontWeight={"600"} fontSize={"sm"}>
-                      {`${ward.firstName} ${ward.lastName}`}
-                    </Text>
-                    <Text
-                      fontSize={"12px"}
-                      color={"#AAAAAA"}
-                      fontWeight={"600"}
-                    >
-                      {ward.greynoteNumber}
-                    </Text>
-                  </Box>
-                </Flex>
-              );
-            })}
-          </ModalBody>
-          <ModalFooter></ModalFooter>
-        </ModalContent>
-      </Modal>
       {children}
     </MainNav>
   );
