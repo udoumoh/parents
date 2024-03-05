a2967c8beb6d8b518810b5cf022cd781d0c403d4
John <johnudoumoh63@gmail.com>
2024-03-05 10:41:41 +0100

    .

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,26 @@
 "use client";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode } from "react";
 import MainNav from "@/components/navigation/mainNav";
 import { useQuery } from "@apollo/client";
 import { GET_PARENT } from "@/gql/queries/queries";
-import { useRouter } from "next/navigation";
-import { Center, Box, Flex, Image, useToast } from "@chakra-ui/react";
+import {
+  Center,
+  Box,
+  Flex,
+  Image,
+  useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { BarLoader } from "react-spinners";
-import { useUserAPI } from "@/hooks/UserContext";
 
 interface layoutProps {
   children: ReactNode;
@@ -15,7 +29,7 @@ interface layoutProps {
 const Layout: React.FC<layoutProps> = ({ children }) => {
   const toast = useToast();
   const { data: parent, loading } = useQuery(GET_PARENT);
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   try {
     if (loading)
       return (
@@ -49,7 +63,34 @@ const Layout: React.FC<layoutProps> = ({ children }) => {
       status: "error",
     });
   }
-  return <MainNav>{children}</MainNav>;
+
+  const Overlay = () => (
+    <ModalOverlay
+      bg="blackAlpha.300"
+      backdropFilter="blur(10px) hue-rotate(90deg)"
+    />
+  );
+
+  return (
+    <MainNav>
+      {!localStorage.getItem("currentId") && (
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          <Overlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text>Custom backdrop filters!</Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      )}
+      {children}
+    </MainNav>
+  );
 };
 
 export default Layout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import UserApiProvider from "@/hooks/UserContext";
 import { Providers } from "./providers";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./globals.css";
+import { Box, Center, Image, Flex } from "@chakra-ui/react";
+import { BarLoader } from "react-spinners";
 
 const client = new ApolloClient({
   uri: "https://api.greynote.app/graphql",
@@ -19,7 +21,26 @@ export default function RootLayout({
   // Check if window is defined before rendering
   if (typeof window === "undefined") {
     // Return placeholder or loading state for server-side rendering
-    return <div>Loading...</div>;
+    return (
+      <Center>
+        <Box minW="full" mt={{ base: 60, md: 60, lg: 40 }}>
+          <Flex
+            direction="column"
+            align="center"
+            minW={{ base: "full", lg: "650px" }}
+          >
+            <Image
+              src="/images/greylightBordered.svg"
+              alt="logo"
+              w={40}
+              mb={3}
+              pointerEvents={"none"}
+            />
+            <BarLoader color="#ffd880" width="150px" />
+          </Flex>
+        </Box>
+      </Center>
+    );
   }
 
   // If on the client side, render the main layout
