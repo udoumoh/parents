934f8292631f03aacd8fc904e5f4e29f423d6c0b
John <johnudoumoh63@gmail.com>
2024-03-05 13:09:00 +0100

    .

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -214,6 +214,7 @@ const SettingsPage: FC<SettingsPageProps> = ({}) => {
                       rounded={"md"}
                       py={"0.5rem"}
                       pl={"1rem"}
+                      pr={'3rem'}
                     >
                       <Avatar
                         size={"md"}
