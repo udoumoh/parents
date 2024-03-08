a3f12b72f452bac131bd2df84de392775e1f6d56
John <johnudoumoh63@gmail.com>
2024-03-08 09:23:45 +0100

    .

---
+++ src/app/loading.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Flex, Image } from "@chakra-ui/react";
+import BarLoader from "react-spinners/BarLoader";
+
+const Loading = () => {
+  return (
+    <Flex
+      justify="center"
+      h="100vh"
+      bgSize="cover"
+      bgRepeat={"no-repeat"}
+      //   bgImage="/images/agentbg.png"
+    >
+      <Flex
+        mt={"10%"}
+        direction="column"
+        align="center"
+        minW={{ base: "full", lg: "650px" }}
+      >
+        <Image
+          src="/images/greylightBordered.svg"
+          alt="Greynote Parents Logo"
+          w="15%"
+          mb={10}
+        />
+        <BarLoader color="#007C7B" width="150px" />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Loading;
