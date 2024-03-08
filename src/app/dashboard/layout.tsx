4c814d3f70ecce892186b0d6ecba9164af150913
John <johnudoumoh63@gmail.com>
2024-03-08 09:47:33 +0100

    .

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,10 +3,6 @@ import React, { ReactNode, useEffect } from "react";
 import MainNav from "@/components/navigation/mainNav";
 import { useQuery } from "@apollo/client";
 import { GET_PARENT } from "@/gql/queries";
-import { useRouter } from "next/navigation";
-import { Center, Box, Flex, Image, useToast } from "@chakra-ui/react";
-import { BarLoader } from "react-spinners";
-import { useUserAPI } from "@/hooks/UserContext";
 import Loading from "../loading";
 
 interface layoutProps {
@@ -14,25 +10,14 @@ interface layoutProps {
 }
 
 const Layout: React.FC<layoutProps> = ({ children }) => {
-  const toast = useToast();
   const { data: parent, loading } = useQuery(GET_PARENT);
 
-  try {
-    if (loading)
-      return (
-        <Loading/>
-      );
-    if (!loading && parent?.parent?.errors !== null) window.location.replace("/signin");
-  } catch (e: any) {
-    toast({
-      title: "Error",
-      description: e.message,
-      position: "top-right",
-      variant: "left-accent",
-      isClosable: true,
-      status: "error",
-    });
-  }
+  useEffect(() => {
+    if (loading) {
+      <Loading/>
+    } else if (!loading && parent?.parent?.errors !== null) {window.location.replace("/signin")}
+  })
+
   return <MainNav>{children}</MainNav>;
 };
 
