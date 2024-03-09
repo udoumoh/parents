0387a02e77685337d9b82b43e91bcf80f0c78026
John <johnudoumoh63@gmail.com>
2024-03-09 10:14:23 +0100

    .

--- src/components/shared/uploadResultModal/index.tsx
+++ src/components/shared/uploadResultModal/index.tsx
@@ -284,7 +284,7 @@ const UploadResultModal: FC<UploadResultModalProps> = ({
                     borderRadius: "8px",
                   },
                 }}
-                h={"400px"}
+                maxH={"400px"}
                 overflowY={"auto"}
                 mt={"0.3rem"}
                 p={"0.5rem"}
