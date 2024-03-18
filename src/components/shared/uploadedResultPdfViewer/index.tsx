4642e2b9f6de7591ff76829724f13814ceb505fd
John <johnudoumoh63@gmail.com>
2024-03-18 12:56:42 +0100

    .

--- src/components/shared/uploadedResultPdfViewer/index.tsx
+++ src/components/shared/uploadedResultPdfViewer/index.tsx
@@ -23,7 +23,7 @@ export const PDFViewer = ({ isOpen, onClose, path }: any) => {
         <ModalHeader>Student Academic Result</ModalHeader>
         <ModalCloseButton />
         <ModalBody overflow="auto" maxH="30rem">
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js">
             <Viewer fileUrl={path} onZoom={handleZoom} />
           </Worker>
         </ModalBody>
