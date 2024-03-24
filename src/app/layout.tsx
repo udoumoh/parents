d3c86d632ae9b3132f5649f5f5c2bdb5c869e73c
John <johnudoumoh63@gmail.com>
2024-03-24 12:45:38 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 "use client";
 import "./globals.css";
+import { FC, useState, useEffect } from "react";
 import UserApiProvider from "@/hooks/UserContext";
 import { Mulish } from "next/font/google";
 import { Providers } from "./providers";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./globals.css";
 import Loading from "./loading";
+import LoadingBar from "react-top-loading-bar";
+import { Router } from "next/router";
+import TopBarProgress from "react-topbar-progress-indicator";
 
 const mulish = Mulish({ subsets: ["cyrillic"] });
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
 const client = new ApolloClient({
   uri: "https://api.greynote.app/graphql",
@@ -15,11 +23,21 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const Layout: FC<LayoutProps> = ({ children }) => {
+  const [progress, setProgress] = useState(false);
+
+  Router.events.on("routeChangeStart", () => {
+    setProgress(true);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    setProgress(false);
+  });
+  TopBarProgress.config({
+    barColors: {
+      "0": "#099C9B",
+      "1.0": "#007C7B",
+    },
+  });
   return (
     <html lang="en">
       <head>
@@ -28,14 +46,19 @@ export default function RootLayout({
       <body className={mulish.className}>
         <ApolloProvider client={client}>
           <Providers>
+            {progress && <TopBarProgress />}
             {typeof window === "undefined" ? (
               <Loading />
             ) : (
-              <UserApiProvider>{children}</UserApiProvider>
+              <UserApiProvider>
+                {children}
+              </UserApiProvider>
             )}
           </Providers>
         </ApolloProvider>
       </body>
     </html>
   );
-}
+};
+
+export default Layout;
---
+++ src/components/shared/progessBar/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ProgressBarProps {
+  
+}
+
+const ProgressBar: FC<ProgressBarProps> = ({}) => {
+  return <div>ProgressBar</div>
+}
+
+export default ProgressBar
