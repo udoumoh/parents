ecbdd124bc7bc40354e045fe9ea4023074026029
John <johnudoumoh63@gmail.com>
2024-03-25 13:32:59 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 "use client";
 import "./globals.css";
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 import UserApiProvider from "@/hooks/UserContext";
 import { Mulish } from "next/font/google";
 import { Providers } from "./providers";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./globals.css";
 import Loading from "./loading";
-import LoadingBar from "react-top-loading-bar";
-import { Router } from "next/router";
 
 const mulish = Mulish({ subsets: ["cyrillic"] });
 
@@ -23,13 +21,7 @@ const client = new ApolloClient({
 });
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  const [progress, setProgress] = useState(0);
-  Router.events.on("routeChangeStart", ()=>{
-    setProgress(10);
-  })
-  Router.events.on("routeChangeComplete", () => {
-    setProgress(100);
-  })
+  
   return (
     <html lang="en">
       <head>
@@ -42,11 +34,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
               <Loading />
             ) : (
               <UserApiProvider>
-                <LoadingBar
-                  color="#f11946"
-                  progress={progress}
-                  onLoaderFinished={() => setProgress(0)}
-                />
                 {children}
               </UserApiProvider>
             )}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,10 @@
 // app/providers.tsx
 "use client";
+import { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { Metadata } from "next";
+import LoadingBar from "react-top-loading-bar";
+import { Router } from "next/router";
 
 
 export const metadata: Metadata = {
@@ -9,5 +12,21 @@ export const metadata: Metadata = {
   description: "",
 };
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  const [progress, setProgress] = useState(0);
+  Router.events.on("routeChangeStart", () => {
+    setProgress(10);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    setProgress(100);
+  });
+  return (
+    <ChakraProvider>
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      {children}
+    </ChakraProvider>
+  );
 }
