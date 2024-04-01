72d1631c5b91984a42dd6fc41eb1359d2c1a74e5
John <johnudoumoh63@gmail.com>
2024-04-01 20:06:53 +0100

    .

--- src/app/dashboard/home/results/page.tsx
+++ src/app/dashboard/home/results/page.tsx
@@ -145,6 +145,7 @@ const Results: FC<ResultsProps> = ({}) => {
           console.log("failed to fetch results data");
         }
         if (response) {
+          console.log(response)
           const parsedResultsData = response?.studentResult?.results?.map(
             (item: any) => ({
               term: item.academicTerm,
