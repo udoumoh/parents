f2f08de10ae3b94aeab0ddc25671a10fe75464dc
John <johnudoumoh63@gmail.com>
2024-04-01 20:22:20 +0100

    .

--- src/app/dashboard/home/results/page.tsx
+++ src/app/dashboard/home/results/page.tsx
@@ -146,7 +146,7 @@ const Results: FC<ResultsProps> = ({}) => {
         }
         if (response) {
           console.log(response)
-          const parsedResultsData = response?.studentUploadedResult?.results?.map(
+          const parsedResultsData = response?.studentUploadedResult?.map(
             (item: any) => ({
               term: item.academicTerm,
               examType: item.resultType,
