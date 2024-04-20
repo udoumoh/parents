9afc88c26d14e1c88806a6f62de4e107b8bb9b51
John <johnudoumoh63@gmail.com>
2024-04-20 18:38:00 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/",
-  sw: "/service-worker.js",
-});
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/",
+    sw: "/service-worker.js",
+})
+const nextConfig = {
+    webpack: (config) => {
+    config.resolve.alias.canvas = false;
 
-module.exports = withPWA(nextConfig);
+    return config;
+ },
+}
+
+module.exports = withPWA(nextConfig)
