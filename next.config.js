64c59e6c4f4798b2f4d1d100132d2f2bff147d23
John <johnudoumoh63@gmail.com>
2024-04-20 18:31:19 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
-  reactStrictMode: true,
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  sw: "/service-worker.js",
 });
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
