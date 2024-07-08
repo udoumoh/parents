f37ce9d8b21480deb4e872d90a258a897659793d
John <johnudoumoh63@gmail.com>
2024-07-08 11:38:16 +0100

    :edt

--- src/app/dashboard/home/invoice/page.tsx
+++ src/app/dashboard/home/invoice/page.tsx
@@ -388,6 +388,7 @@ const Invoice: FC<InvoiceProps> = ({}) => {
                             gap={"3"}
                             onClick={() => {
                               handleInvoiceDataModal(item?.id);
+                              onInvoiceDataModalOpen();
                             }}
                           >
                             <Icon
